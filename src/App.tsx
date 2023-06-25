d93d72060a43406cc8a358ad56b39238f8423939
eladsadeh <elad.sadeh1@.gmail.com>
2023-06-24 22:33:15 -0400

    contentEditable element with controled text and placement of caret

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, useEffect} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('Some text')
+  const caretLocation = useRef(0);
 
+  useEffect(() => {
+    // console.log("UseEffect:", caretLocation.current, text);
+    const el = document.getElementById("text");
+    const sel = window.getSelection();
+    if (el && sel) {
+      // console.log(el.innerHTML);
+      const range = document.createRange();
+      // console.log(caretLocation.current, el.childNodes[0]);
+      range.setStart(el.childNodes[0], caretLocation.current);
+      range.collapse(true);
+      sel.removeAllRanges();
+      sel.addRange(range);
+      // el.focus();
+    }
+  }, [text]);
+
+  function handleChange() {
+    const sel = window.getSelection();
+    if (sel) {
+      console.log(sel.anchorOffset);
+      caretLocation.current = sel.anchorOffset;
+    }
+    const el = document.getElementById("text");
+    el && setText(el.innerHTML);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <span
+        id="text"
+        contentEditable={true}
+        onBeforeInput={(ev) => console.log("beforeInput", ev)}
+        dangerouslySetInnerHTML={{ __html: text }}
+        onInput={handleChange}
+      >
+      </span>
   )
 }
 
